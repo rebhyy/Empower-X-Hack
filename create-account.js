5e899999096b358e7560d4ac74aebb1cf8b08a08
ed-marquez <72571340+ed-marquez@users.noreply.github.com>
2022-08-09 17:13:54 -0400

    Changes for blog post
    - Changes to create-account.js for modularity
    - Changes to .env for user convenience

--- create-account.js
+++ create-account.js
@@ -1,84 +1,78 @@
+console.clear();
 const {
-  Client,
-  PrivateKey,
-  Hbar,
-  TransferTransaction,
-  AccountId,
+	Client,
+	PrivateKey,
+	Hbar,
+	TransferTransaction,
+	AccountId,
+	TransactionRecordQuery,
 } = require("@hashgraph/sdk");
-const dotenv = require("dotenv");
+require("dotenv").config();
 const axios = require("axios");
-dotenv.config();
 const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
+const operatorId = AccountId.fromString(process.env.OPERATOR_ID);
+const operatorKey = PrivateKey.fromString(process.env.OPERATOR_PVKEY);
+const client = Client.forTestnet().setOperator(operatorId, operatorKey);
+
 async function main() {
-  //Grab your Hedera testnet account ID and private key from your .env file
-  const myAccountId = process.env.ACCOUNT_ID;
-  const myPrivateKey = process.env.PRIVATE_KEY;
+	// Generate ECDSA key pair
+	console.log("- Generating a new key pair... \n");
+	const newPrivateKey = PrivateKey.generateECDSA();
+	const newPublicKey = newPrivateKey.publicKey;
+	const newAliasAccountId = newPublicKey.toAccountId(0, 0);
 
-  // If we weren't able to grab it, we should throw a new error
-  if (myAccountId == null || myPrivateKey == null) {
-    throw new Error(
-      "Environment variables myAccountId and myPrivateKey must be present"
-    );
-  }
+	console.log(`- New account alias: ${newAliasAccountId} \n`);
+	console.log(`- New private key (Hedera): ${newPrivateKey} \n`);
+	console.log(`- New public key (Hedera): ${newPublicKey} \n`);
+	console.log(`- New private key (RAW EVM): 0x${newPrivateKey.toStringRaw()} \n`);
+	console.log(`- New public key (RAW): 0x${newPublicKey.toStringRaw()} \n`);
+	console.log(`- New public key (EVM): 0x${newPublicKey.toEthereumAddress()} \n\n`);
 
-  // Create our connection to the Hedera network
-  // The Hedera JS SDK makes this really easy!
-  //Create your local client
-  const client = Client.forTestnet();
+	// Transfer HBAR to newAliasAccountId to auto-create the new account
+	// Get account information from a transaction record query
+	const [txReceipt, txRecQuery] = await autoCreateAccountFcn(operatorId, newAliasAccountId, 100);
+	console.log(`- HBAR Transfer to new account: ${txReceipt.status} \n\n`);
+	console.log(`- Parent transaction ID: ${txRecQuery.transactionId} \n`);
+	console.log(`- Child transaction ID: ${txRecQuery.children[0].transactionId.toString()} \n`);
+	console.log(
+		`- New account ID (from RECORD query): ${txRecQuery.children[0].receipt.accountId.toString()} \n`
+	);
 
-  client.setOperator(myAccountId, myPrivateKey);
-
-  // Create ECDSA keypair
-  console.log("Generating a new keypair");
-  const newAccountPrivateKey = await PrivateKey.generateECDSA();
-  const publicKey = newAccountPrivateKey.publicKey;
-
-  // Assuming that the target shard and realm are known.
-  // For now they are virtually always 0 and 0.
-  const aliasAccountId = publicKey.toAccountId(0, 0);
-  console.log("Account alias", aliasAccountId.toString());
-
-  // Sending Hbar to account alias to auto-create account
-  const sendHbar = await new TransferTransaction()
-    .addHbarTransfer(myAccountId, Hbar.from(-100)) //Sending account
-    .addHbarTransfer(aliasAccountId, Hbar.from(100)) //Receiving account
-    .execute(client);
-  const transactionReceipt = await sendHbar.getReceipt(client);
-  console.log(
-    "The transfer transaction from my account to the new account was: " +
-      transactionReceipt.status.toString()
-  );
-
-  await delay(10000); // wait for 10 seconds before querying account id
-  const mirrorNodeUrl = "https://testnet.mirrornode.hedera.com/api/v1/";
-  try {
-    const account = await axios.get(
-      mirrorNodeUrl +
-        "accounts?account.publickey=" +
-        newAccountPrivateKey.publicKey.toStringRaw()
-    );
-    console.log("New account id", account.data?.accounts[0].account);
-  } catch (err) {
-    console.log(err);
-  }
-
-  console.log(
-    "New account private key (EVM)",
-    "0x" + newAccountPrivateKey.toStringRaw()
-  );
-  console.log(
-    "New account public key (EVM)",
-    "0x" + newAccountPrivateKey.publicKey.toEthereumAddress()
-  );
-
-  console.log(
-    "New account private key (Hedera)",
-    newAccountPrivateKey.toString()
-  );
-  console.log(
-    "New account public key (Hedera)",
-    newAccountPrivateKey.publicKey.toString()
-  );
+	// Get account information from a mirror node query
+	const mirrorQueryResult = await mirrorQueryFcn(newPublicKey);
+	console.log(
+		`- New account ID (from MIRROR query): ${mirrorQueryResult.data?.accounts[0].account} \n`
+	);
 }
+
+async function autoCreateAccountFcn(senderAccountId, receiverAccountId, hbarAmount) {
+	//Transfer hbar to the account alias to auto-create account
+	const transferToAliasTx = new TransferTransaction()
+		.addHbarTransfer(senderAccountId, new Hbar(-hbarAmount))
+		.addHbarTransfer(receiverAccountId, new Hbar(hbarAmount))
+		.freezeWith(client);
+	const transferToAliasSign = await transferToAliasTx.sign(operatorKey);
+	const transferToAliasSubmit = await transferToAliasSign.execute(client);
+	const transferToAliasRx = await transferToAliasSubmit.getReceipt(client);
+
+	// Get a transaction record and query the record to get information about the account creation
+	const transferToAliasRec = await transferToAliasSubmit.getRecord(client);
+	const txRecordQuery = await new TransactionRecordQuery()
+		.setTransactionId(transferToAliasRec.transactionId)
+		.setIncludeChildren(true)
+		.execute(client);
+	return [transferToAliasRx, txRecordQuery];
+}
+
+async function mirrorQueryFcn(publicKey) {
+	// Query a mirror node for information about the account creation
+	await delay(10000); // Wait for 10 seconds before querying account id
+	const mirrorNodeUrl = "https://testnet.mirrornode.hedera.com/api/v1/";
+	const mQuery = await axios.get(
+		mirrorNodeUrl + "accounts?account.publickey=" + publicKey.toStringRaw()
+	);
+	return mQuery;
+}
+
 main();
